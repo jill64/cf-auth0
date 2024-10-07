caab6c078cd0a51ef3ee2c8c950370debc34e964
jill64 <intents.turrets0h@icloud.com>
2024-10-07 17:16:47 +0900

    Exclude cjs directory and dist from TypeScript compilation

--- eslint.config.js
+++ eslint.config.js
@@ -2,5 +2,6 @@ import { tsConfig } from '@jill64/eslint-config-ts'
 
 /** @type {import('@jill64/eslint-config-ts').FlatConfig[]} */
 export default tsConfig({
-  tsConfigPath: './eslint.tsconfig.json'
+  tsConfigPath: './eslint.tsconfig.json',
+  ignores: ['**/cjs/**', 'dist']
 })
