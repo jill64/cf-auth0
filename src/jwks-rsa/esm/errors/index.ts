58ecb017e6c04303a0131bbeb3245e20087dd978
jill64 <intents.turrets0h@icloud.com>
2024-10-07 11:18:55 +0900

    fix: jwks error export

--- src/jwks-rsa/esm/errors/index.ts
+++ src/jwks-rsa/esm/errors/index.ts
@@ -3,9 +3,4 @@ import JwksError from './JwksError.js'
 import JwksRateLimitError from './JwksRateLimitError.js'
 import SigningKeyNotFoundError from './SigningKeyNotFoundError.js'
 
-export default {
-  ArgumentError,
-  JwksError,
-  JwksRateLimitError,
-  SigningKeyNotFoundError
-}
+export { ArgumentError, JwksError, JwksRateLimitError, SigningKeyNotFoundError }
