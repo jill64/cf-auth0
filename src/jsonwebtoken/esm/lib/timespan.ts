7760e66daee1773110f7ffec5d7963723499259e
jill64 <intents.turrets0h@icloud.com>
2024-10-07 11:05:24 +0900

    define jwt utils

---
+++ src/jsonwebtoken/esm/lib/timespan.ts
@@ -0,0 +1,17 @@
+import ms from 'ms'
+
+export default function (time: Parameters<typeof ms>[0], iat: number) {
+  const timestamp = iat || Math.floor(Date.now() / 1000)
+
+  if (typeof time === 'string') {
+    var milliseconds = ms(time)
+    if (typeof milliseconds === 'undefined') {
+      return
+    }
+    return Math.floor(timestamp + milliseconds / 1000)
+  } else if (typeof time === 'number') {
+    return timestamp + time
+  } else {
+    return
+  }
+}
