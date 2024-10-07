2364f354f947d14efba6e93274d03593601f207a
jill64 <intents.turrets0h@icloud.com>
2024-10-07 17:13:47 +0900

    Refactor callbackSupport function to accept optional kid parameter

--- src/jwks-rsa/esm/wrappers/callbackSupport.ts
+++ src/jwks-rsa/esm/wrappers/callbackSupport.ts
@@ -4,7 +4,10 @@ import { JwksClient } from '../JwksClient.js'
 const callbackSupport = (client: JwksClient) => {
   const getSigningKey = client.getSigningKey.bind(client)
 
-  return (kid: unknown, cb: (err: Error | null, key: unknown) => void) => {
+  return (
+    kid: string | undefined,
+    cb: (err: Error | null, key: unknown) => void
+  ) => {
     if (cb) {
       const callbackFunc = callbackify(getSigningKey)
       return callbackFunc(kid, cb)
