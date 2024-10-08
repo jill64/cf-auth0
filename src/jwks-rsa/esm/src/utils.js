314a88980df96c5a876772adaa8d95f349342040
jill64 <intents.turrets0h@icloud.com>
2024-10-08 17:22:04 +0900

    Refactor retrieveSigningKeys function in utils.js

--- src/jwks-rsa/esm/src/utils.js
+++ src/jwks-rsa/esm/src/utils.js
@@ -77,8 +77,10 @@ async function retrieveSigningKeys(jwks) {
         // Assume legacy Node.js version without the Symbol.toStringTag backported
         // Fall through
         default:
+          // getSpki = () => key.export({ format: 'pem', type: 'spki' })
           // @ts-expect-error TODO
-          getSpki = () => key.export({ format: 'pem', type: 'spki' })
+          var spki = await exportSPKI(key)
+          getSpki = () => spki
       }
       results.push({
         get publicKey() {
