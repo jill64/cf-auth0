aff90b730a57816da9c3c426b60fd73836410c72
jill64 <intents.turrets0h@icloud.com>
2024-10-10 02:26:50 +0900

    Refactor toString function to use arrow function

--- src/jws/esm/lib/tostring.ts
+++ src/jws/esm/lib/tostring.ts
@@ -1,6 +1,6 @@
 import { Buffer } from 'node:buffer'
 
-export default function toString(obj: unknown) {
+export default (obj: unknown) => {
   if (typeof obj === 'string') return obj
   if (typeof obj === 'number' || Buffer.isBuffer(obj)) return obj.toString()
   return JSON.stringify(obj)
