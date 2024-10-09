5932b1382d82cc85cb7c0d3c5a0a8bae1bcc38d6
jill64 <intents.turrets0h@icloud.com>
2024-10-09 23:54:37 +0900

    Refactor verify function to remove unnecessary callback and improve error handling

--- src/jwks-rsa/esm/src/JwksClient.ts
+++ src/jwks-rsa/esm/src/JwksClient.ts
@@ -1,104 +1,59 @@
-import debug from 'debug'
 import JwksError from './errors/JwksError.js'
 import SigningKeyNotFoundError from './errors/SigningKeyNotFoundError.js'
 import { retrieveSigningKeys } from './utils.js'
-import {
-  cacheSigningKey,
-  callbackSupport,
-  getKeysInterceptor,
-  rateLimitSigningKey,
-  request
-} from './wrappers/index.js'
 
-const logger = debug('jwks')
+export const JwksClient = (jwksUri: string) => {
+  const getKeys = async () => {
+    console.log(`Fetching keys from '${jwksUri}'`)
 
-class JwksClient {
-  // @ts-expect-error TODO
-  constructor(options) {
-    // @ts-expect-error TODO
-    this.options = {
-      rateLimit: false,
-      cache: true,
-      timeout: 30000,
-      ...options
+    const resp = await fetch(jwksUri)
+
+    if (!resp.ok) {
+      const errorMsg =
+        (await resp.text()) ?? resp.statusText ?? `Http Error ${resp.status}`
+
+      throw new Error(errorMsg)
     }
 
-    // Initialize wrappers.
-    // @ts-expect-error TODO
-    if (this.options.getKeysInterceptor) {
-      this.getSigningKey = getKeysInterceptor(this, options)
-    }
+    const res = await resp.json()
 
-    // @ts-expect-error TODO
-    if (this.options.rateLimit) {
-      this.getSigningKey = rateLimitSigningKey(this, options)
-    }
-    // @ts-expect-error TODO
-    if (this.options.cache) {
-      this.getSigningKey = cacheSigningKey(this, options)
-    }
+    console.log('Keys:', res.keys)
 
-    // @ts-expect-error TODO
-    this.getSigningKey = callbackSupport(this, options)
+    return res.keys
   }
 
-  async getKeys() {
-    // @ts-expect-error TODO
-    logger(`Fetching keys from '${this.options.jwksUri}'`)
-
-    try {
-      const res = await request({
-        // @ts-expect-error TODO
-        uri: this.options.jwksUri,
-        // @ts-expect-error TODO
-        headers: this.options.requestHeaders
-      })
-
-      logger('Keys:', res.keys)
-      return res.keys
-    } catch (err) {
-      // @ts-expect-error TODO
-      const { errorMsg } = err
-      logger('Failure:', errorMsg || err)
-      throw errorMsg ? new JwksError(errorMsg) : err
-    }
-  }
-
-  async getSigningKeys() {
-    const keys = await this.getKeys()
+  const getSigningKeys = async () => {
+    const keys = await getKeys()
 
     if (!keys || !keys.length) {
       throw new JwksError('The JWKS endpoint did not contain any keys')
     }
 
-    // eslint-disable-next-line no-undef
     console.log('Keys2:', keys)
 
     const signingKeys = await retrieveSigningKeys(keys)
 
-    // eslint-disable-next-line no-undef
     console.log('Signing Keys:', signingKeys)
 
     if (!signingKeys.length) {
       throw new JwksError('The JWKS endpoint did not contain any signing keys')
     }
 
-    logger('Signing Keys:', signingKeys)
+    console.log('Signing Keys:', signingKeys)
     return signingKeys
   }
 
-  // @ts-expect-error TODO
-  async getSigningKey(kid) {
-    logger(`Fetching signing key for '${kid}'`)
+  const getSigningKey = async (kid: string) => {
+    console.log(`Fetching signing key for '${kid}'`)
 
-    const keys = await this.getSigningKeys()
+    const keys = await getSigningKeys()
 
     // eslint-disable-next-line no-undef
     console.log('Keys:', keys)
 
     const kidDefined = kid !== undefined && kid !== null
     if (!kidDefined && keys.length > 1) {
-      logger('No KID specified and JWKS endpoint returned more than 1 key')
+      console.log('No KID specified and JWKS endpoint returned more than 1 key')
       throw new SigningKeyNotFoundError(
         'No KID specified and JWKS endpoint returned more than 1 key'
       )
@@ -111,12 +66,15 @@ class JwksClient {
 
       return key
     } else {
-      logger(`Unable to find a signing key that matches '${kid}'`)
+      console.log(`Unable to find a signing key that matches '${kid}'`)
       throw new SigningKeyNotFoundError(
         `Unable to find a signing key that matches '${kid}'`
       )
     }
   }
-}
 
-export { JwksClient }
+  return {
+    getSigningKey,
+    getSigningKeys
+  }
+}
