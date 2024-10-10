fb77f3bad7c59379177be7a117d9b7d0fa44a2ae
jill64 <intents.turrets0h@icloud.com>
2024-10-10 15:46:49 +0900

    Refactor createPrivateKey and createPublicKey to support JWK format

--- src/lib/crypto/createPublicKey.ts
+++ src/lib/crypto/createPublicKey.ts
@@ -1,5 +1,35 @@
-// import type { createPublicKey as CreatePublicKey } from 'node:crypto'
+import type { createPublicKey as CreatePublicKey } from 'node:crypto'
+import { KeyObjectLike } from './KeyObjectLike.js'
+import { subtle } from './index.js'
+import { prepareAsymmetricKey } from './internal/prepareAsymmetricKey.js'
 
-// export const createPublicKey: typeof CreatePublicKey = () => {}
+export const createPublicKey = async (
+  key: Parameters<typeof CreatePublicKey>[0] | KeyObjectLike
+): Promise<KeyObjectLike> => {
+  const result = prepareAsymmetricKey(key, 'kCreatePublic')
 
-export { createPublicKey } from 'node:crypto'
+  const { format, data } = result
+  const jwk = 'jwk' in data ? data.jwk : null
+
+  if (format === 'jwk') {
+    if (jwk === null) {
+      throw new TypeError('ERR_MISSING_VALUE: key.jwk')
+    }
+
+    if (!jwk.alg) {
+      throw new TypeError('ERR_MISSING_ARG: key.jwk.alg')
+    }
+
+    const key = await subtle.importKey(
+      'jwk',
+      jwk,
+      jwk.alg,
+      true,
+      (jwk.key_ops as KeyUsage[]) ?? []
+    )
+
+    return KeyObjectLike.from(key)
+  }
+
+  throw new TypeError('Unsupported key format')
+}
