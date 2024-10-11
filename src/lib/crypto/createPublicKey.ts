8248793b1d3887074b5112d6dd8981608e45ba4b
jill64 <intents.turrets0h@icloud.com>
2024-10-11 16:10:27 +0900

    Refactor import statement in createPublicKey.ts

--- src/lib/crypto/createPublicKey.ts
+++ src/lib/crypto/createPublicKey.ts
@@ -1,55 +1,53 @@
-// import type {
-//   createPublicKey as CreatePublicKey,
-//   KeyObject as KeyObjectType
-// } from 'node:crypto'
-// import { subtle } from './index.js'
-// import { prepareAsymmetricKey } from './internal/prepareAsymmetricKey.js'
-// import { KeyObject } from './KeyObject.js'
+import type {
+  createPublicKey as CreatePublicKey,
+  KeyObject as KeyObjectType
+} from 'node:crypto'
+import { subtle } from './index.js'
+import { prepareAsymmetricKey } from './internal/prepareAsymmetricKey.js'
+import { KeyObject } from './KeyObject.js'
 
-// export const createPublicKey = async (
-//   key: Parameters<typeof CreatePublicKey>[0]
-// ): Promise<KeyObjectType> => {
-//   const result = prepareAsymmetricKey(key, 'kCreatePublic')
+export const createPublicKey = async (
+  key: Parameters<typeof CreatePublicKey>[0]
+): Promise<KeyObjectType> => {
+  const result = prepareAsymmetricKey(key, 'kCreatePublic')
 
-//   const { format, data } = result
-//   const jwk = 'jwk' in data ? data.jwk : null
+  const { format, data } = result
+  const jwk = 'jwk' in data ? data.jwk : null
 
-//   if (format === 'jwk') {
-//     if (jwk === null) {
-//       throw new TypeError('ERR_MISSING_VALUE: key.jwk')
-//     }
+  if (format === 'jwk') {
+    if (jwk === null) {
+      throw new TypeError('ERR_MISSING_VALUE: key.jwk')
+    }
 
-//     if (!jwk.alg) {
-//       throw new TypeError('ERR_MISSING_ARG: key.jwk.alg')
-//     }
+    if (!jwk.alg) {
+      throw new TypeError('ERR_MISSING_ARG: key.jwk.alg')
+    }
 
-//     const key = await subtle.importKey(
-//       'jwk',
-//       jwk,
-//       jwk.alg,
-//       true,
-//       (jwk.key_ops ?? []) as KeyUsage[]
-//     )
+    const key = await subtle.importKey(
+      'jwk',
+      jwk,
+      jwk.alg,
+      true,
+      (jwk.key_ops ?? []) as KeyUsage[]
+    )
 
-//     return KeyObject.from(key) as KeyObjectType
-//   }
+    return KeyObject.from(key) as KeyObjectType
+  }
 
-//   if (format === 'spki') {
-//     const key = await subtle.importKey(
-//       'spki',
-//       data,
-//       {
-//         name: 'RSA-PSS',
-//         hash: 'SHA-256'
-//       },
-//       true,
-//       ['verify']
-//     )
+  if (format === 'spki') {
+    const key = await subtle.importKey(
+      'spki',
+      data,
+      {
+        name: 'RSA-PSS',
+        hash: 'SHA-256'
+      },
+      true,
+      ['verify']
+    )
 
-//     return KeyObject.from(key) as KeyObjectType
-//   }
+    return KeyObject.from(key) as KeyObjectType
+  }
 
-//   throw new TypeError('Unsupported key format')
-// }
-
-export { createPublicKey } from 'node:crypto'
+  throw new TypeError('Unsupported key format')
+}
