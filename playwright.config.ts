3f9afc1d41919d164cb26b5304f3c4cc3f9f2fad
jill64 <contact@jill64.dev>
2025-01-07 23:30:25 +0900

    fix: update playwright configuration to set workers to 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,6 @@
-import { extendsConfig, branchPreview } from '@jill64/playwright-config'
+import { branchPreview, extendsConfig } from '@jill64/playwright-config'
 
-export default extendsConfig(branchPreview())
+export default extendsConfig({
+  ...branchPreview(),
+  workers: 1
+})
